78aa466c872203d8e7f27e5662288995f4197e7d
Taylor Anderson <taylor-anderson@hotmail.ca>
2018-09-27 17:55:56 -0400

    got rid of pixelartscaler dependency cuz laptop was bein real mad about it and i dont need it

--- source/Main.hx
+++ source/Main.hx
@@ -2,39 +2,35 @@ import com.haxepunk.Engine;
 import com.haxepunk.HXP;
 import haxepunk.Graphic;
 import haxepunk.debug.Console;
-import haxepunk.pixel.PixelArtScaler;
+// import haxepunk.pixel.PixelArtScaler;
 import openfl.Lib;
 import openfl.display.FPS;
 
-
-class Main extends Engine
-{
+class Main extends Engine {
 	public function new() {
-		super(240, 160,60, false);
-		
+		super(240, 160, 60, false);
 	}
-	override public function init()
-	{
 
-#if debug
+	override public function init() {
+		#if debug
 		Console.enable();
-#end
-		
+		#end
+
 		Graphic.smoothDefault = false;
 		Graphic.pixelSnappingDefault = true;
-		//PixelArtScaler.globalActivate();
-		
+		// PixelArtScaler.globalActivate();
+
 		HXP.resize(960, 640);
 		HXP.scene = new TitleScene();
-		
+
 		var fps:FPS = new FPS(10, 10, 0);
 		var format = fps.defaultTextFormat;
 		format.size = 20;
 		fps.defaultTextFormat = format;
-		//Lib.current.stage.addChild(fps);
+		// Lib.current.stage.addChild(fps);
 	}
 
-	public static function main() { new Main(); }
-	
-
+	public static function main() {
+		new Main();
+	}
 }
